47373ffb97868526412f66c63af10fe500e327f3
Johan Camilo Calderon Alvarez <jjcalderon.4697@unicesmag.edu.co>
2025-06-05 11:38:41 -0500

    Requerimiento final

---
+++ src/backend/signin.php
@@ -0,0 +1,37 @@
+<?php
+
+    include('../../config/database.php');
+
+    session_start();
+    if(!isset($_SESSION['user_id'])) {
+        header('Refresh:0; url=http://localhost/schoolar2/src/index.html');
+    }
+
+    $email = $_POST ['e_mail'];
+    $passw = $_POST ['p_sswd'];
+    $enc_past = sha1($passw);
+
+    $sql = "select id,
+	COUNT(id) as total
+from 
+	users 
+where 
+	email = '$email' and 
+	password = '$enc_past' and 
+	status = true
+    GROUP BY id";
+
+    $res = pg_query($conn, $sql);
+
+    if($res){
+        $row = pg_fetch_assoc($res);
+        if($row['total'] > 0){
+            //echo "Login ok";
+            $_SESSION['user_id'] = $row['id'];
+            header('Refresh:0; url=http://localhost/schoolar2/src/index.html');
+        } else {
+            echo "Login failed";
+        }
+    }
+
+?>
---
+++ src/list_users.php
@@ -0,0 +1,58 @@
+<?php
+ include('../config/database.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border="1" aling="center">
+        <tr>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>E-mail</th>
+            <th>Status</th>
+            <th>Photo</th>
+            <th>.....   </th>
+        </tr>
+       
+        <?php
+            //codigo
+           $sql = 
+           "select 
+                id,
+                firstname, 
+                lastname, 
+                email, 
+                case when status = true then 'Active' else 'No active' end as status  
+           from 
+                users;";
+
+            $res = pg_query($conn, $sql);
+            if(!$res){
+                    echo "Query error";
+                    exit;
+            }
+
+            while($row = pg_fetch_assoc($res)){
+                echo "<tr>";
+                echo "<td>". $row['firstname'] ."</td>";
+                echo "<td>". $row['lastname'] ."</td>";
+                echo "<td>". $row['email'] ."</td>";
+                echo "<td>". $row['status'] ."</td>";
+                echo "<td align = center><img src='photo_users/photo_default.png' width= '35' ></td>";
+                echo "<td>";
+                echo "<a href=''><img src= 'icons/edit/curriculum.png' width='40'></a>";
+                echo "<a href=''><img src= 'icons/edit/images (1).png' width='40'></a>";
+                echo "<a href='http://127.0.0.1/schoolar/src/delete.php'><img src= 'icons/edit/2622200.png' width='40'></a>";
+                echo "</td>";
+                echo "</tr>";
+            }
+        ?>
+    </table>
+</body>
+</html>
